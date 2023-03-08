a8b1cf57149b1ea73804860d8152e5a09f6f7494
Vimal Badoniya <107095697+VimalBadoniya@users.noreply.github.com>
2023-03-08 16:42:39 +0530

    Add files via upload
    Delete button added for deleting the expense (final functionality is not done for delete button)

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import ExpenseDate from "./components/ExpenseDate";
 import ExpenseDetails from "./components/ExpenseDetails";
-import "./components/ExpenseDetails.css";
+import "./styles/ExpenseElement.css";
+import React from "react";
 
-function App() {
+let App = () => {
   let data = [
     {
       date: new Date(2022, 2, 15).toISOString(),
@@ -25,57 +26,32 @@ function App() {
     },
   ];
 
-  let ExKey = 500;
-  let Getkey = () => {
-    ExKey++;
-    return ExKey;
+  let DivKey = 500;
+  let GetDivKey = () => {
+    DivKey++;
+    return DivKey;
   };
 
-  let AllExpenseDetails = [];
+  let clickHandler = () =>{
+    console.log("Delete button clicked")
+  }
+
+  let Alldiv = [];
   for (let i = 0; i <= 2; i++) {
-    AllExpenseDetails.push(
-      <div key={i}>
+    Alldiv.push(
+      <div className="expense-item" id={GetDivKey()}>
         <ExpenseDetails
-          key={i}
           Expense_amount={data[i].Amount}
           Expense_details={data[i].Details}
         ></ExpenseDetails>
-        <ExpenseDate key={Getkey()} Expense_date={data[i].date}></ExpenseDate>
+        <ExpenseDate Expense_date={data[i].date}></ExpenseDate>
+      <button onClick={clickHandler} className="expense-item__price">Delete</button>
       </div>
     );
   }
 
-  return (
-    <div className="App">
-      <h1>Expense Tracker</h1>
-      <div className="container">
-        <ExpenseDetails
-          key={0}
-          Expense_amount={data[0].Amount}
-          Expense_details={data[0].Details}
-        ></ExpenseDetails>
-        <ExpenseDate key={Getkey()} Expense_date={data[0].date}></ExpenseDate>
-      </div>
-
-      <div className="container">
-        <ExpenseDetails
-          key={1}
-          Expense_amount={data[1].Amount}
-          Expense_details={data[1].Details}
-        ></ExpenseDetails>
-        <ExpenseDate key={Getkey()} Expense_date={data[1].date}></ExpenseDate>
-      </div>
-
-      <div className="container">
-        <ExpenseDetails
-          key={2}
-          Expense_amount={data[2].Amount}
-          Expense_details={data[2].Details}
-        ></ExpenseDetails>
-        <ExpenseDate key={Getkey()} Expense_date={data[2].date}></ExpenseDate>
-      </div>
-    </div>
-  );
+  return Alldiv;
 }
+
 
 export default App;
