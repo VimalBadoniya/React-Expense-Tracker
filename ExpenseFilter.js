48e4577871c8fffead480634781e755e1efa2eda
Vimal Badoniya <107095697+VimalBadoniya@users.noreply.github.com>
2023-03-15 22:38:54 +0530

    Add files via upload
    Filter function added and we have added some conditional statements also

--- ExpenseFilter.js
+++ ExpenseFilter.js
@@ -1,14 +1,17 @@
-import "../ExpenseInput/ExpenseForm.css"
+import "../ExpenseInput/ExpenseForm.css";
 
 let ExpenseFilter = (props) => {
-    let dropDownChangeHandler = (event)=>{
-        props.onYearChange(event.target.value)
-        console.log(event.target.value)
-    }
+  let dropDownChangeHandler = (event) => {
+    props.onYearChange(event.target.value);
+  };
   return (
     <div className="new-expense">
       <label htmlFor="ExpenseFilter">Filter By Year</label>
-      <select id="ExpenseFilter" value={props.selectedYear} onChange={dropDownChangeHandler}>
+      <select
+        id="ExpenseFilter"
+        value={props.selectedYear}
+        onChange={dropDownChangeHandler}
+      >
         <option value="2020">2020</option>
         <option value="2021">2021</option>
         <option value="2022">2022</option>
